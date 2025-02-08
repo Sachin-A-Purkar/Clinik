02b4c26cdf5acb08b2f98cada8661dfa8f14c910
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-02-08 16:05:30 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter } from "react-router-dom";
 import './App.css'
 
 import Nav from './Landing/Nav'
+import Carousle from './Landing/Carousle'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Nav/>
+    <BrowserRouter>
+    <Nav/>
+    </BrowserRouter>
+      
+      {/* <Carousle/> */}
     </>
   )
 }
--- src/Landing/Nav.jsx
+++ src/Landing/Nav.jsx
@@ -1,43 +1,239 @@
-import React from 'react'
-import "./Nav.css"
-import { useState } from 'react'
-import { useEffect } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import { CgMenuRightAlt } from "react-icons/cg";
+// import "./Nav.css";
 
+// function Nav() {
+//   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+//   const toggleMenu = () => {
+//     setIsMenuOpen(!isMenuOpen);
+//   };
 
-export default function Nav() {
-  const[time,setTime]=useState(new Date().toLocaleTimeString())
+//   const closeMenu = () => {
+//     setIsMenuOpen(false); // Hide the menu when a navigation item is clicked
+//   };
 
-  useEffect(()=>{
-    const interval=setInterval(()=>{
-      setTime(new Date().toLocaleTimeString());
-    }, 1000)
-  })
-  return (
-    <div className='navbar'>
-      <div className='clock'>
-        <p>
-          Current Time : {time}
-        </p>
-      </div>
-      <div className='headbar'>
-            <div className='part1'>
-                <img src="/heart.png" className='himg1'/>
-                <h2 className='text1'>Apex Hospital</h2>
-            </div>
-            <div className='part2'>
-                <a href="#home" className='an1'> HOME </a>
-                <a href="#home" className='an1'> ABOUT </a>
-                <a href="#home" className='an1'> SERVICES </a>
-                <a href="#home" className='an1'> PAGES </a>
-                <a href="#home" className='an1'> CONTACT </a>
-            </div>
-            <div className='part3'>
-              <button>Appointment<FontAwesomeIcon icon={faArrowRight}/></button>
-            </div>
-      </div>
+//   return (
+//     <>
+//     <header>
+//       <div id='navbar'>
+//         <div id='navbar-left'>
+//           <img src="/heart.png" className='logoimg' alt='Logo'/>
+//           <p>Apex Hospital</p>
+//         </div>
+//         <div id='navbar-right' className={isMenuOpen ? "open-menu" : ""}>
+//           <ul>
+//             <li>
+//               <a href='#home'>Home</a>
+//             </li>
+//             <li>
+//               <a href='#services'>Services</a>
+//             </li>
+//             <li>
+//               <a href='#about'>About Us</a>
+//             </li>
+//             <li>
+//               <a href='#contact'>Contact</a>
+//             </li>
+//           </ul>
+//           </div>
+//           <div id="options" className={isMenuOpen ? "open-menu" : ""}>
+//           <ul>
+//             <li>
+//               <Link to="/login" onClick={closeMenu}>
+//                 Login / Register &nbsp;|
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/booktable" onClick={closeMenu}>
+//                 Book Table
+//               </Link>
+//             </li>
+//           </ul>
+//           <button >Book Appointment</button>
+//         </div>
+          
+        
+//       </div>
       
-    </div>
-  )
-}
+
+//       <div className="rightmenu" onClick={toggleMenu}>
+//           <ul>
+//             <li>
+//               <a id="menu-item">
+//                 <i className="fa-solid fa-bars"></i>
+//               </a>
+//               <ul className={`megamenu ${isMenuOpen ? "open-megamenu" : ""}`}>
+//                 <li>
+//                   <a href="#home" onClick={closeMenu}>
+//                     Home
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a href="#about" onClick={closeMenu}>
+//                     About
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a href="#menu" onClick={closeMenu}>
+//                     Menu
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a href="#laurels" onClick={closeMenu}>
+//                     Awards
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <a href="#find-us" onClick={closeMenu}>
+//                     Contact
+//                   </a>
+//                 </li>
+//                 <li>
+//                   <Link to="/login" onClick={closeMenu}>
+//                     Login / Register
+//                   </Link>
+//                 </li>
+//                 <li>
+//                   <Link to="/booktable" onClick={closeMenu}>
+//                     Book Table
+//                   </Link>
+//                 </li>
+//               </ul>
+//             </li>
+//           </ul>
+//         </div>
+//     </header>
+//     </>
+//   );
+// }
+
+// export default Nav;
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+const Nav = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false); // Hide the menu when a navigation item is clicked
+  };
+
+  return (
+    <>
+      <div id="navbar">
+        <div id="image">
+          <img id="navimg" src="heart.png" alt="Logo" />
+          <p>Apex Hospital</p>
+        </div>
+
+
+
+
+        <div id="menuitems" className={isMenuOpen ? "open-menu" : ""}>
+          <ul>
+            <li>
+              <a href="#home" onClick={closeMenu}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" onClick={closeMenu}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#menu" onClick={closeMenu}>
+                Menu
+              </a>
+            </li>
+            <li>
+              <a href="#laurels" onClick={closeMenu}>
+                Awards
+              </a>
+            </li>
+            <li>
+              <a href="#find-us" onClick={closeMenu}>
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="#login" onClick={closeMenu}>
+              Login / Register 
+              </a>
+            </li>
+          </ul>
+        </div>
+
+
+
+        <div id="options" className={isMenuOpen ? "open-menu" : ""}>
+          <button to="/booktable" onClick={closeMenu}>Book Appointment</button>
+        </div>
+
+
+
+
+
+        <div className="rightmenu" onClick={toggleMenu}>
+          <ul>
+            <li>
+              <a id="menu-item">
+                <i className="fa-solid fa-bars"></i>
+              </a>
+              <ul className={`megamenu ${isMenuOpen ? "open-megamenu" : ""}`}>
+                <li>
+                  <a href="#home" onClick={closeMenu}>
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#about" onClick={closeMenu}>
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a href="#menu" onClick={closeMenu}>
+                    Menu
+                  </a>
+                </li>
+                <li>
+                  <a href="#laurels" onClick={closeMenu}>
+                    Awards
+                  </a>
+                </li>
+                <li>
+                  <a href="#find-us" onClick={closeMenu}>
+                    Contact
+                  </a>
+                </li>
+                <li>
+                  <Link to="/login" onClick={closeMenu}>
+                    Login / Register
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/booktable" onClick={closeMenu}>
+                    Book Table
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+
+
+
+      </div>
+    </>
+  );
+};
+
+export default Nav;
