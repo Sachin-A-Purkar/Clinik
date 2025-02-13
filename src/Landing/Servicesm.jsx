54887da8177cf95310b52518a6f0da252d6f2e64
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-02-13 10:40:30 +0530

    service

---
+++ src/Landing/Servicesm.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import "./Servicesm.css"
+import { FaHeartbeat , FaBrain ,FaWheelchair ,FaTooth ,FaVials} from "react-icons/fa";
+import { FaXRay } from "react-icons/fa";
+
+function Servicesm() {
+  return (
+    <div>
+      <div className="hea">
+        <button className='but4'>About Us</button>
+        <h2 className='heati'>Health Care Solutions</h2>
+      </div>
+      <div className="container  serc">
+        <div className="row serv">
+          <div className="column">
+            <div className='box1'>
+              <div className="card">
+              <FaHeartbeat id='ico'/>
+                <div className="card-body">
+                  <h2 className="card-title">Card Title</h2>
+                  <p className="card-text">This is a simple card component with some text inside it.</p>
+                  <button className="card-btn">Read More</button>
+                </div>
+              </div>
+            </div>
+            <div className='box2'>
+            <div className="card">
+            <FaXRay id='ico'/>
+                <div className="card-body">
+                  <h2 className="card-title">Card Title</h2>
+                  <p className="card-text">This is a simple card component with some text inside it.</p>
+                  <button className="card-btn">Read More</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className='box1'>
+            <div className="card">
+            <FaBrain id='ico'/>
+                <div className="card-body">
+                  <h2 className="card-title">Card Title</h2>
+                  <p className="card-text">This is a simple card component with some text inside it.</p>
+                  <button className="card-btn">Read More</button>
+                </div>
+              </div>
+            </div>
+            <div className='box2'>
+            <div className="card">
+              <FaWheelchair id='ico'/>
+                <div className="card-body">
+                  <h2 className="card-title">Card Title</h2>
+                  <p className="card-text">This is a simple card component with some text inside it.</p>
+                  <button className="card-btn">Read More</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="column">
+            <div className='box1'>
+            <div className="card">
+                <FaTooth id='ico'/>
+                <div className="card-body">
+                  <h2 className="card-title">Card Title</h2>
+                  <p className="card-text">This is a simple card component with some text inside it.</p>
+                  <button className="card-btn">Read More</button>
+                </div>
+              </div>
+            </div>
+            <div className='box2'>
+            <div className="card">
+            <FaVials id='ico'/>
+                <div className="card-body">
+                  <h2 className="card-title">Card Title</h2>
+                  <p className="card-text">This is a simple card component with some text inside it.</p>
+                  <button className="card-btn">Read More</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Servicesm
+
+{/* <button className='but2'>About Us</button><br/> */}
