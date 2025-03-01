e9bc9021c8563ba8bf845d58b834edc01e1a3cde
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-03-01 14:04:38 +0530

    update

--- src/Landing/Features.jsx
+++ src/Landing/Features.jsx
@@ -13,8 +13,15 @@ const Features = () => {
           <h1 className='feath1'>Why Choose Us</h1>
           <p className='featp'>At the heart of our hospital is a team of highly skilled and experienced medical professionals dedicated to delivering the highest standard of care. Our doctors, nurses, and healthcare staff are not only leaders in their respective fields but also deeply committed to providing personalized and compassionate care to every patient.</p>
           <div className='row featr1'>
-            <div className='col featc1'><FontAwesomeIcon icon={faUserDoctor} border  id='fico'/><FontAwesomeIcon icon={faCheck} id='fico'/></div>
-            <div className='col featc2'><FontAwesomeIcon icon={faCommentMedical} id='fico'/><FontAwesomeIcon icon={faHeadphonesSimple} id='fico'/></div>
+            <div className='col featc1'>
+            <p className='cona'><FontAwesomeIcon icon={faUserDoctor} border  id='fico'/> Expirenced <br/> Doctor</p>
+              <br/>
+            <p className='cona'><FontAwesomeIcon icon={faCommentMedical} border id='fico'/>Positive <br/>Consultation</p>
+              </div>
+            <div className='col featc2'>
+                <FontAwesomeIcon icon={faCheck} border id='fico'/><h2 id='fih2'>Quality <br/>Services</h2>
+                <FontAwesomeIcon icon={faHeadphonesSimple} border id='fico'/><h2 id='fih2'>24Hours <br/>Support</h2>
+            </div>
           </div>
         </div>
         <div className="col-sm-12 col-md p-3' coluu2">
